70872dbdbd01ce05d044b42b27a712c43671f19d
EstLladon <estlladon@gmail.com>
2023-03-20 18:06:44 +0000

    added CSSaccess

---
+++ Automation/FileDistribution/CSSaccess.py
@@ -0,0 +1,112 @@
+import dpop_utils
+import json, requests, urllib.parse, base64
+
+
+class CSSaccess:
+    """
+    Provides access to a CSS solid server.
+    """
+    def __init__(self, IDP,USERNAME,PASSWORD):
+        self.idp = IDP
+        self.username = USERNAME
+        self.password = PASSWORD
+        self.cred_url = IDP+'idp/credentials/'
+        self.token_url = IDP+'.oidc/token'
+        self.authstring =''
+        self.authtoken = ''
+        self.dpopKey = dpop_utils.generate_dpop_key_pair()
+        
+    def __repr__(self):
+        """
+        Return IDP. Can be changed later
+        """
+        return str(self.idp)
+    
+    def create_authstring(self):
+        #issuer_url = IDP
+        # create a token 
+        #auth = (client_id,client_secret)
+        #headers= {'content-type': 'text/plain'}
+        data ={ 'email': self.username, 'password': self.password, 'name': 'my-token' }
+        datajson=json.dumps(data)
+        res = requests.post(self.cred_url, headers={ 'content-type': 'application/json' }, data=str(datajson))
+        res=res.json()
+        self.authstring=urllib.parse.quote(res['id'])+':'+urllib.parse.quote(res['secret'])
+        return self.authstring
+    
+    def create_authtoken(self):
+        s=bytes(self.authstring, 'utf-8')
+        auth='Basic %s' % str(base64.standard_b64encode(s))[2:]
+        res = requests.post(
+            self.token_url,
+            headers= {
+                'content-type': 'application/x-www-form-urlencoded',
+                'authorization': auth,
+                'DPoP': dpop_utils.create_dpop_header(self.token_url, 'POST', self.dpopKey)
+            },
+            data={"grant_type":"client_credentials","scope": "webid"},
+            timeout=5000
+        )
+        #print(res.json())
+        self.authtoken =res.json()['access_token']
+        return self.authtoken
+
+    def put_file(self,podname,filename,filetext,filetype):
+        targetUrl=self.idp+podname+'/'+filename
+        headers={ 'content-type': filetype, 'authorization':'DPoP '+self.authtoken, 'DPoP': dpop_utils.create_dpop_header(targetUrl, "PUT", self.dpopKey)}
+        res= requests.put(targetUrl,
+            headers=headers,
+            data=filetext
+        )
+        return(res.text)
+        
+    def get_file(self,targetUrl):
+        #targetUrl='http://localhost:3000/test1/file1.txt'
+        headers={  'authorization':'DPoP '+self.authtoken, 'DPoP': dpop_utils.create_dpop_header(targetUrl, "GET", self.dpopKey)}
+
+        res= requests.get(targetUrl,
+           headers=headers
+        )
+        return res.text
+
+
+
+
+
+
+def putdirCSS (directory,pod,IDP,USERNAME,PASSWORD):
+    client_id = USERNAME
+    client_secret = PASSWORD
+
+    # The server that provides your account (where you login)
+    issuer_url = IDP
+    # create a token 
+    auth = (client_id,client_secret)
+    #headers= {'content-type': 'text/plain'}
+    data ={ 'email': client_id, 'password': client_secret, 'name': 'my-token' }
+    datajson=json.dumps(data)
+    cred_url=issuer_url+'idp/credentials/'
+    res = requests.post(cred_url, headers={ 'content-type': 'application/json' }, data=str(datajson))
+    res=res.json()
+    authstring=urllib.parse.quote(res['id'])+':'+urllib.parse.quote(res['secret'])
+    #print(authstring)
+    dpopKey = dpop_utils.generate_dpop_key_pair()
+    tokenUrl = issuer_url+'.oidc/token'
+    s=bytes(authstring, 'utf-8')
+    #auth=f'Basic {base64.standard_b64encode(s)}'
+    auth='Basic %s' % str(base64.standard_b64encode(s))[2:]
+    print(auth)
+    #auth = f'Basic ${Buffer.from(authString).toString('base64')}'
+    res = requests.post(
+        tokenUrl,
+        headers= {
+            'content-type': 'application/x-www-form-urlencoded',
+            'authorization': auth,
+            'DPoP': dpop_utils.create_dpop_header(tokenUrl, 'POST', dpopKey)
+        },
+        #data= 'grant_type=client_credentials&scope=webid',
+        data={"grant_type":"client_credentials","scope": "webid"},
+        timeout=5000
+    )
+    #print(res.json())
+    authtoken =res.json()['access_token']
