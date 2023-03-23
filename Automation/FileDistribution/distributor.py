30599f19f40a22f2751f8a69c8a537432f196202
EstLladon <estlladon@gmail.com>
2023-03-23 15:54:33 +0000

    cssaccess update

--- Automation/FileDistribution/distributor.py
+++ Automation/FileDistribution/distributor.py
@@ -3,6 +3,7 @@ from solid_client_credentials import SolidClientCredentialsAuth, DpopTokenProvid
 import dpop_utils
 import requests
 import os, json
+import CSSaccess
 
 from solid.auth import Auth
 
@@ -26,7 +27,7 @@ def putdirCSS (directory,pod,IDP,USERNAME,PASSWORD,indexfile=''):
             #api.put_file(file_url, filetext, 'text/markdown')
     indexpath=os.path.join(directory, indexfile)
     if os.path.isfile(indexpath):
-        CSSA.put_file(pod, filename, filetext, 'text/turtle')
+        CSSA.put_file(pod, indexfile, filetext, 'text/turtle')
     return pod
     
 
@@ -55,3 +56,21 @@ def postdirNSS (directory,pod,IDP,USERNAME,PASSWORD):
 
     api = SolidAPI(auth)
     postdirtopod(directory,pod,api)
+
+
+class Pod:
+    """
+    Class to work with pods.
+    """
+    def __init__(self, IDP,USERNAME,PASSWORD,podname):
+        self.idp = IDP
+        self.username = USERNAME
+        self.password = PASSWORD
+        self.podname=podname
+        self.Access=CSSaccess.CSSaccess(IDP, USERNAME, PASSWORD)
+
+    def __repr__(self):
+        """
+        Return IDP. Can be changed later
+        """
+        return str(self.idp+self.podname)
