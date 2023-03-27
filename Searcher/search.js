f5198baf1e5b1bc7a15bb0ee373e826437ac134d
Mohamed Ragab <39013653+MohamedRagabAnas@users.noreply.github.com>
2023-03-27 16:23:45 +0100

    SPARQL Syntax for Search App

--- Searcher/search.js
+++ Searcher/search.js
@@ -5,17 +5,45 @@ const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const myEngine = new QueryEngine();
 
 app.get('/', async (req,res) => {
-    const bindingsStream = await myEngine.queryBindings(`
-  SELECT ?o WHERE 
-  {?s ?p "Boom".
-  ?s ?p ?o
-     } LIMIT 100`, {
+    const bindingsStream = await myEngine.queryBindings(
+        `
+        PREFIX ns1: <http://example.org/SOLID/>
+        SELECT  ?address ?frequency
+        WHERE {
+        ?x ns1:appearsIn
+        [ ns1:address ?address ;
+        ns1:frequency ?frequency ] ;
+        ns1:lemma 'coffee'.
+        } LIMIT 10
+        `, {
         sources: ['https://storage.inrupt.com/271193f6-926b-45c1-8fa8-b31cc03accb4/index.ttl'],
     });
 
     const bindings = await bindingsStream.toArray();
     res.json(bindings)
 })
+
+
+//Yury's Solution after Fixing Syntax
+// app.get('/', async (req,res) => {
+//     const bindingsStream = await myEngine.queryBindings(
+//         `
+//         PREFIX ns1: <http://example.org/SOLID/>
+//         SELECT  ?a ?f
+//         WHERE {
+//            ?s1 ns1:lemma 'coffee'.
+//            ?s1 ns1:appearsIn ?s2.
+//            ?s2 ns1:address ?a.
+//            ?s2 ns1:frequency ?f.
+//         } LIMIT 100
+//         `, {
+//         sources: ['https://storage.inrupt.com/271193f6-926b-45c1-8fa8-b31cc03accb4/index.ttl'],
+//     });
+//
+//     const bindings = await bindingsStream.toArray();
+//     res.json(bindings)
+// })
+
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
