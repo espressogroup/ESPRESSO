dbacf590c747fc97223692b5a446e5b5db01752d
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2023-04-07 16:45:47 +0100

    Barista remove unnecessary classes ...

--- Barista/otherclasses/Repository.java
+++
@@ -1,9 +0,0 @@
-package org.espresso.barista;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface Repository extends JpaRepository <SolidIndex,Long>{
-
-    List<SolidIndex> findByNameContaining(String keyword);
-}
