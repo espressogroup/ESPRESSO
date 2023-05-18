55b9a052b506e39becdc927adc00b121efff78a0
Mohamed Ragab <39013653+MohamedRagabAnas@users.noreply.github.com>
2023-05-18 17:34:29 +0100

    Update experiments.sh

--- GaianDB/Gaian_Maven/installConfig/experiments.sh
+++ GaianDB/Gaian_Maven/installConfig/experiments.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Set the path to the file containing keywords
-keyword_file="keywords.txt"
+keyword_file="keyword.txt"
 
 # For Unix, when double-clicking on this script, we want the home+workspace dirs to be the install location (NOT the user's home folder!):
 [[ `dirname $0` != "$PWD" ]] && [[ -z "$GDBH" ]] && export GDBH=`dirname $0` && [[ -z "$GAIAN_WORKSPACE" ]] && export GAIAN_WORKSPACE=$GDBH
 
 [[ -z $GDBH ]] && export GDBH=.
 
+# Read the list of keywords from the file
+readarray -t keywords < "$keyword_file"
+
 # Loop over each keyword in the file
-while read keyword; do
+for keyword in "${keywords[@]}"; do
   # Generate the SQL command for the current keyword
-  sql_command="SELECT * FROM LTSOLID WHERE term LIKE '%$keyword%'"
+  sql_command="SELECT * FROM LTSOLID WHERE term = '$keyword'"
+
 
   # Measure the execution time of the SQL command
   start_time=$(date +%s.%N)
-  $GDBH/queryDerby.sh $* "$sql_command" > /dev/null
+  $GDBH/queryDerby.sh  "$sql_command" > /dev/null
   end_time=$(date +%s.%N)
   elapsed_time=$(echo "scale=6; $end_time - $start_time" | bc)
 
@@ -24,10 +28,3 @@ while read keyword; do
   echo "Execution time: $elapsed_time seconds"
   echo
 done < $keyword_file
-
-
-
-
-
-
-
