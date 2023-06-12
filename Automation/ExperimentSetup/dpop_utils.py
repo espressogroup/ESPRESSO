0f45621ab21309496e0957874e9fad7597f3ba6b
EstLladon <estlladon@gmail.com>
2023-06-12 17:41:56 +0100

    dpop remove

--- Automation/ExperimentSetup/dpop_utils.py
+++ Automation/ExperimentSetup/dpop_utils.py
@@ -17,38 +17,38 @@ def generate_dpop_key_pair() -> jwk.JWK:
     return key
 
 
-def refresh_access_token(
-    token_endpoint: str,
-    current_token: AccessToken | None,
-    client_id: str,
-    client_secret: str,
-    dpop_key: jwk.JWK,
-    refresh_before_expiration_seconds: int,
-) -> AccessToken:
-    if current_token is None or current_token.expires_within(
-        refresh_before_expiration_seconds
-    ):
-        return request_access_token(token_endpoint, client_id, client_secret, dpop_key)
-    return current_token
+# def refresh_access_token(
+#     token_endpoint: str,
+#     current_token: AccessToken | None,
+#     client_id: str,
+#     client_secret: str,
+#     dpop_key: jwk.JWK,
+#     refresh_before_expiration_seconds: int,
+# ) -> AccessToken:
+#     if current_token is None or current_token.expires_within(
+#         refresh_before_expiration_seconds
+#     ):
+#         return request_access_token(token_endpoint, client_id, client_secret, dpop_key)
+#     return current_token
 
 
-def request_access_token(
-    token_endpoint: str, client_id: str, client_secret: str, dpop_key: jwk.JWK
-) -> AccessToken:
-    res = requests.post(
-        token_endpoint,
-        auth=(client_id, client_secret),
-        headers={
-            "DPoP": create_dpop_header(token_endpoint, "POST", dpop_key),
-        },
-        data={"grant_type": "client_credentials", "scope": "webid"},
-        timeout=5000,
-    )
-    access_token = res.json()["access_token"]
-    token_payload = jwt_decode_without_verification(access_token)
-    expiration = datetime.datetime.fromtimestamp(token_payload["exp"])
+# def request_access_token(
+#     token_endpoint: str, client_id: str, client_secret: str, dpop_key: jwk.JWK
+# ) -> AccessToken:
+#     res = requests.post(
+#         token_endpoint,
+#         auth=(client_id, client_secret),
+#         headers={
+#             "DPoP": create_dpop_header(token_endpoint, "POST", dpop_key),
+#         },
+#         data={"grant_type": "client_credentials", "scope": "webid"},
+#         timeout=5000,
+#     )
+#     access_token = res.json()["access_token"]
+#     token_payload = jwt_decode_without_verification(access_token)
+#     expiration = datetime.datetime.fromtimestamp(token_payload["exp"])
 
-    return AccessToken(access_token, expiration=expiration)
+#     return AccessToken(access_token, expiration=expiration)
 
 
 def create_dpop_header(url: str, method: str, key: jwk.JWK) -> str:
