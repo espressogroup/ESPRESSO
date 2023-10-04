e34034e1447a3e3a81ea5e652c928045ec9448b5
EstLladon <estlladon@gmail.com>
2023-10-04 14:47:44 +0100

    int

--- Automation/DatasetSplitter/smartchunk.py
+++ Automation/DatasetSplitter/smartchunk.py
@@ -49,4 +49,4 @@ max_combined_size_bytes = max_combined_size_mb * 1024 * 1024
 
     #split_text_file(output_file, output_dir, num_files)
     #os.remove(output_file)
-smartchunk(input_dir,output_dir,num_files,max_combined_size_bytes)
+smartchunk(input_dir,output_dir,int(num_files),int(max_combined_size_bytes))
