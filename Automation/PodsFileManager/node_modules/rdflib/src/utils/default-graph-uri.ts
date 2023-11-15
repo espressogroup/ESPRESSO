1f2f3886d0ec4d9e3723e1fe6e7a591fc948615f
MohamedRagabAnas <mohamed.ragab@nub.edu.eg>
2023-11-15 10:30:52 +0000

    clean main Automation

--- Automation/PodsFileManager/node_modules/rdflib/src/utils/default-graph-uri.ts
+++
@@ -1,6 +0,0 @@
-// Prevents circular dependencies between data-factory-internal and statement
-
-import NamedNode from '../named-node'
-
-export const defaultGraphURI = 'chrome:theSession'
-export const defaultGraphNode = new NamedNode(defaultGraphURI)
